c9bad7228f8dd51dddd0bfd083d0a4e3824c2384
Piero Viano <piero.viano@gmail.com>
2022-11-13 01:05:14 +0200

    ''

--- Build.Debug.bat
+++ Build.Debug.bat
@@ -3,8 +3,8 @@ del ..\WhenTheVersion\Packages\FreeImage-dotnet-core.*
 rmdir /s /q %userprofile%\.nuget\Packages\FreeImage-dotnet-core
 nuget restore FreeImage-dotnet-core.sln
 MSBuild.exe FreeImage-dotnet-core.sln /property:Configuration=Debug 
-copy "FreeImage-dotnet-core\bin\Debug\FreeImage-dotnet-core.1.0.0.nupkg" ..\WhenTheVersion\Packages\
-copy "FreeImage-dotnet-core\bin\Debug\FreeImage-dotnet-core.1.0.0.symbols.nupkg" ..\WhenTheVersion\Packages\
+copy "FreeImage-dotnet-core\bin\Debug\FreeImage-dotnet-core.4.3.7.nupkg" ..\WhenTheVersion\Packages\
+copy "FreeImage-dotnet-core\bin\Debug\FreeImage-dotnet-core.4.3.7.symbols.nupkg" ..\WhenTheVersion\Packages\
 git add -A
 git commit -a --allow-empty-message -m ''
 git push
