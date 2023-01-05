9a01f818b4c194f0d23a6580601e26e01a150906
Piero Viano <piero.viano@gmail.com>
2023-01-05 03:46:22 +0200

    ''

---
+++ Build.bat
@@ -0,0 +1,6 @@
+IF NOT DEFINED Configuration SET Configuration=Release
+MSBuild.exe FreeImage-dotnet-core.sln -t:restore -p:RestorePackagesConfig=true
+MSBuild.exe FreeImage-dotnet-core.sln -m /property:Configuration=%Configuration%
+git add -A
+git commit -a --allow-empty-message -m ''
+git push
