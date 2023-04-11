7bd018d6be72924d14a0fe7f8ac38040267496ca
Piero Viano <piero.viano@gmail.com>
2023-04-11 07:16:46 +0300

    ''

--- Build.bat
+++ Build.bat
@@ -1,4 +1,6 @@
+mklink /j Packages ..\WhenTheVersion\Packages
 IF NOT DEFINED Configuration SET Configuration=Release
+MSBuild.exe FreeImage-dotnet-core.sln -t:clean
 MSBuild.exe FreeImage-dotnet-core.sln -t:restore -p:RestorePackagesConfig=true
 MSBuild.exe FreeImage-dotnet-core.sln -m /property:Configuration=%Configuration%
 git add -A
